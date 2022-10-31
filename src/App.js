6e4f3c2350271bc13fae288eb098afb1ce4eaaee
Wellington Rodrigues <wellingtonrodrigues.rj@gmail.com>
2022-10-31 14:39:32 -0300

    .

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <ul>
-        <li>Identador de JSON</li>
-        <li>Gerador de CPF</li>
-        <li>Mimificador</li>
-      </ul>
     </div>
   );
 }
