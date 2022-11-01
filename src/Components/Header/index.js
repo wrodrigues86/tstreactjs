998017a77d0736ca426da6ea4dc46c9cf1a0961b
Wellington Rodrigues <wellingtonrodrigues.rj@gmail.com>
2022-11-01 16:10:28 -0300

    Update index.js

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Menu(){
     return(
         <div>
-            <Link to="/">Home</Link> - 
-            <Link to="sobre">Sobre</Link>
+            <Link to="/">Home</Link>
+            <Link to="/sobre">Sobre</Link>
         </div>
     );
 };
